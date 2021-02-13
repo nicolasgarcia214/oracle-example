ae8fefb2aee476383e81e0d49b6674c5f5370e59
nicolasgarcia214 <nicolasgarcia214@gmail.com>
2021-02-12 19:51:01 -0500

    ✨Consumer, Oracle Interface and deploy file

---
+++ contracts/Consumer.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.3;
+
+import './IOracle.sol'
+
+contract Consumer {
+    IOracle public oracle;
+
+    constructor(address _oracle){
+        oracle = IOracle(_oracle);
+    }
+
+    function foo() external{
+        bytes32 key = keccak256(abi.encodePacked("BTC/USD"));
+
+        (bool result, uint timestamp, uint data) = oracle.getData(key);
+        require(result == true, "Could not get price");
+        require(timestamp >= block.timestamp - 2 minuter, "price to old");
+    }
+}
